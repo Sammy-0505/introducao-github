30eb93d6755371ba922cca1c8d152e97efa434ff
Samuel Salles <147561940+Sammy-0505@users.noreply.github.com>
2025-01-31 18:09:23 +0000

    adiciona o .index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Eu te amo mia princesa.</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-}
-
 export default Home;
